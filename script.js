a5d2108dd21f323f261b4a1f22ec34bacbc16f4a
Rupak <rupakganvir12@gmail.com>
2022-01-21 12:55:39 +0530

    added files

---
+++ script.js
@@ -0,0 +1,104 @@
+console.log("Welcome to my Spotify")
+//initialize the veriable
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Excuses", filePath: "songs/1.mp3", coverPath: "cover/cover1.jpg"},
+    {songName: "Brown Munde", filePath: "songs/2.mp3", coverPath: "cover/cover2.jpg"},
+    {songName: "Bijlee Bijlee", filePath: "songs/3.mp3", coverPath: "cover/cover3.jpg"},
+    {songName: "G.O.A.T.", filePath: "songs/4.mp3", coverPath: "cover/cover4.jpg"},
+    {songName: "Srivalli", filePath: "songs/5.mp3", coverPath: "cover/cover5.jpg"},
+    {songName: "Excuses 2", filePath: "songs/1.mp3", coverPath: "cover/cover1.jpg"},
+    {songName: "Bijlee Bijlee 2", filePath: "songs/3.mp3", coverPath: "cover/cover3.jpg"},
+]
+
+songItems.forEach((element, i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
+
+//handle play pause
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+//listen to event
+audioElement.addEventListener('timeupdate',()=>{
+    // console.log('timeupdate');
+    //update seek bar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    // console.log(progress);
+    myProgressBar.value = progress;
+})
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlay = ()=>{
+    // e.target.classList.add('fa-pause-circle');
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.classList.remove('fa-pause-circle');
+    element.classList.add('fa-play-circle');
+})
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlay();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=6){
+        songIndex=0;
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex=0;
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
